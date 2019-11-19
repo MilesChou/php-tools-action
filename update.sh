e6e235693e8f6c47707985b34f1adb18e5234a46
MilesChou <jangconan@gmail.com>
2019-11-19 23:33:17 +0800

    Add Dockerfile for multiple php version

---
+++ update.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+generated_warning() {
+	cat <<-EOH
+		#
+		# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
+		#
+		# PLEASE DO NOT EDIT IT DIRECTLY.
+		#
+	EOH
+}
+
+build_dockerfile() {
+    echo "Build ./${1}/Dockerfile ..."
+    generated_warning > ./${1}/Dockerfile
+    cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${LATEST_VERSION}-alpine"'!' >> ./${1}/Dockerfile
+
+    for version in ${VERSIONS}; do
+        echo "Build ./${version}/${1}/Dockerfile ..."
+        mkdir -p ${version}/${1}
+
+        generated_warning > ./${version}/${1}/Dockerfile
+        cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${version}-alpine"'!' >> ./${version}/${1}/Dockerfile
+
+        cp ./${1}/entrypoint.sh ${version}/${1}/entrypoint.sh
+        cp ./${1}/docker-install-${1} ${version}/${1}/docker-install-${1}
+    done
+}
+
+LATEST_VERSION=7.3
+
+VERSIONS="
+5.5
+5.6
+7.0
+7.1
+7.2
+7.3
+"
+
+echo "Build Dockerfile for PHP CodeSniffer ..."
+build_dockerfile phpcs
+
+echo "Build Dockerfile for PHPUnit ..."
+build_dockerfile phpunit
