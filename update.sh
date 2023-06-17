2c4d0d3a208e128304aa83e89196c483e7636358
pmcgoverncw <51176015+pmcgoverncw@users.noreply.github.com>
2023-06-16 21:18:12 -0500

    Add php versioning for tests (#5)
    * chore: clean & re-organize tests. Simplify update.sh & testing process to remove duplicate code
    
    * chore: move working test workflow to testing.yaml. Remove unnecessary workflows
    
    * chore: remove unused matrix value
    
    * fix: update update.sh to generate versioned directories & files
    
    * fix: ciorrect modification process for entrypoint & docker-install files
    
    * chore:  adding phpstan to update.sh buildout

--- update.sh
+++ update.sh
@@ -11,42 +11,50 @@ generated_warning() {
 }
 
 build_dockerfile() {
-    echo "Build ./${1}/Dockerfile ..."
-    generated_warning > ./${1}/Dockerfile
+  declare PHP_IMAGES
 
-    for version in ${VERSIONS}; do
-        echo "Build ./${version}/${1}/Dockerfile ..."
-        mkdir -p ${version}/${1}
+  echo "Build ./${1}/Dockerfile ..."
+  generated_warning > ./${1}/Dockerfile
 
-        generated_warning > ./${version}/${1}/Dockerfile
-        cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${version}-alpine"'!' >> ./${version}/${1}/Dockerfile
+  for version in "${versions[@]}"; do
+    echo "Building for $version"
+    # append the container images to use in testing each version
+    quote="'"
+    delimeter=', '
+    PHP_IMAGES+="$quote$version$quote$delimeter"
 
-        cp ./${1}/entrypoint.sh ${version}/${1}/entrypoint.sh
-        cp ./${1}/docker-install-${1} ${version}/${1}/docker-install-${1}
+    # Build the Dockerfile from the supplied template
+    echo "Build ./${version}/${1}/Dockerfile ..."
+    mkdir -p ${version}/${1}
 
-        LATEST_VERSION=${version}
-    done
+    rm -f ./${version}/${1}/Dockerfile
+    generated_warning > ./${version}/${1}/Dockerfile
+    cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${version}-alpine"'!' >> ./${version}/${1}/Dockerfile
+    git add ./${version}/${1}/Dockerfile
 
-    echo "Build ./${1}/Dockerfile using latest version (${LATEST_VERSION})..."
+    rm -f ./${version}/${1}/entrypoint.sh
+    cp ./${1}/entrypoint.sh ./${version}/${1}/entrypoint.sh
+    git add ./${version}/${1}/entrypoint.sh
 
-    rm -f ./${1}/Dockerfile
-    generated_warning > ./${LATEST_VERSION}/${1}/Dockerfile
-    cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${LATEST_VERSION}-alpine"'!' >> ./${1}/Dockerfile
+    rm -f ./${version}/${1}/docker-install-${1}
+    cp ./${1}/docker-install-${1} ./${version}/${1}/docker-install-${1}
+    git add ./${version}/${1}/docker-install-${1}
+  done
 
+#  echo "Build ./${1}/Dockerfile using latest version (${LATEST_VERSION})..."
+#
+#  rm -f ./${1}/Dockerfile
+#  generated_warning > ./${LATEST_VERSION}/${1}/Dockerfile
+#  cat ./${1}/Dockerfile.template | sed -e 's!%%PHP_VERSION%%!'"${LATEST_VERSION}-alpine"'!' >> ./${1}/Dockerfile
+
+  # Update testing GitHub Workflow to include all supported versions
+  PHP_IMAGES=$( printf "%s" "${PHP_IMAGES/%', '/}")
+
+  declare php_image_string="php-image: [${PHP_IMAGES}]"
+  sed -i '' "s/php-image:.*$/$php_image_string/g" .github/workflows/testing.yaml
 }
 
-VERSIONS="
-5.5
-5.6
-7.0
-7.1
-7.2
-7.3
-7.4
-8.0
-8.1
-8.2
-"
+versions=('5.5' '5.6' '7.0' '7.1' '7.2' '7.3' '7.4' '8.0' '8.1' '8.2')
 
 echo "Build Dockerfile for PHP CodeSniffer ..."
 build_dockerfile phpcs
@@ -59,3 +67,6 @@ build_dockerfile phpmetrics
 
 echo "Build Dockerfile for PHPUnit ..."
 build_dockerfile phpunit
+
+echo "Build Dockerfile for PHPStan ..."
+build_dockerfile phpstan
